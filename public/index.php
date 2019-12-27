a61fd27849f58c7e2e799c3b9b8faa7e4d58063b
Jason Cooke <jason.cooke@hotmail.fr>
2019-12-27 17:40:04 +0100

    Minor changes
    Uninstalled Tracy
    Defined a logo for tabs

--- public/index.php
+++ public/index.php
@@ -1,18 +1,13 @@
 <?php
 
 use App\Router;
-use Tracy\Debugger;
 
 require_once '../vendor/autoload.php';
-
-
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
 $router = new Router();
-
-Debugger::enable();
 
 $router->run();
